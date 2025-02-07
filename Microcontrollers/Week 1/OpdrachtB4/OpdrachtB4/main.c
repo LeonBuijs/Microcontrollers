f5a961e6f09040091e5badd0ee4b583d11454dce
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2025-02-07 14:05:01 +0100

    Files added from week 1

---
+++ Microcontrollers/Week 1/OpdrachtB4/OpdrachtB4/main.c
@@ -0,0 +1,38 @@
+/*
+ * OpdrachtB4.c
+ *
+ * Created: 7-2-2025 12:13:03
+ * Author : leonb
+ */ 
+
+#define F_CPU 8e6
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void wait(int ms)
+{
+	for (int i = 0; i < ms; i++)
+	{
+		_delay_ms(1);
+	}
+}
+
+
+int main(void)
+{
+	DDRD = 0xFF;
+
+	while(1)
+	{
+		PORTD = 0b10000000;
+		wait(50);
+
+		for (int i = 0; i < 8; i++)
+		{
+			PORTD = PORTD >> 1;
+			wait(50);
+		}
+	}
+}
+
---
+++ Microcontrollers/Week 1/OpdrachtB6/OpdrachtB6/main.c
@@ -0,0 +1,48 @@
+/*
+ * OpdrachtB6.c
+ *
+ * Created: 7-2-2025 13:02:05
+ * Author : leonb
+ */ 
+
+#define F_CPU 8e6
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
+
+void wait(int ms)
+{
+	for (int i = 0; i < ms; i++)
+	{
+		_delay_ms(1);
+	}
+}
+
+int main(void)
+{
+	bool speed_up = false;
+	
+	DDRD = 0xFF;
+    PORTA=0x01;
+	
+	while (1) 
+    {
+		if(PINA&0x01){
+			speed_up = !speed_up;
+		}
+		if (!speed_up)
+		{
+			PORTD = 0b10000000;
+			wait(1000);
+			PORTD = 0b00000000;
+			wait(1000);
+		} else {
+			PORTD = 0b10000000;
+			wait(250);
+			PORTD = 0b00000000;
+			wait(250);
+		}
+    }
+}
+
